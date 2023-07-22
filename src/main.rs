7249a370308a37ec30fcc8aea19ca7cdf896b0a8
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-07-22 04:08:52 +0100

    Team

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 // mod watermelon;
 // mod way_too_long_string;
 // mod the_man_who_became_a_god;
-mod hamon_odyssey;
+// mod hamon_odyssey;
+mod team;
 
 fn main(){
     // watermelon::main();
     // way_too_long_string::main();
     // the_man_who_became_a_god::main();
-    hamon_odyssey::main();
+    // hamon_odyssey::main();
+    team::main();
+
 }
