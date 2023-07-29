d91f1f372c4349963b2cc5a3e00a2150206065f1
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-07-29 04:43:37 +0100

    B now working but with time limit exceeded error

--- src/a.rs
+++ src/a.rs
@@ -12,24 +12,20 @@ pub fn main() {
             .split_whitespace()
             .map(|x| x.parse().unwrap())
             .collect();
+        let k = nk[1] as usize;
+        let mut res: Vec<String> = vec![String::new(); k];
+
         let mut a = String::new();
-        io::stdin().read_line(&mut a).expect("Error reading line");
-        let a: Vec<u32> = a
-            .split_whitespace()
-            .map(|x| x.parse::<u32>().unwrap() as u32 % nk[1])
-            .collect();
-        let mut res: Vec<String> = vec!["".to_string(); nk[1] as usize];
-        let mut re: String = String::new();
-        for i in 0..a.len() {
-            if a[i] == 0 {
-                re.push_str((i + 1).to_string().as_str());
-                re.push(' ');
-            } else {
-                res[nk[1] as usize - a[i] as usize].push_str((i + 1).to_string().as_str());
-                res[nk[1] as usize - a[i] as usize].push(' ');
+        io::stdin().read_line(&mut a).expect("Error reading line");    
+        a.split_whitespace().enumerate().for_each(|(i, x)| {
+        let val = (x.parse::<u32>().unwrap() as u32) % k as u32;
+            if val == 0 {
+                print!("{} ", i + 1);
+            }  else {
+                res[(nk[1] - val) as usize] += ((i + 1).to_string() + " ").as_str();
+                
             }
-        }
-        let result_string: String = re.to_string() + &res.join("");
-        println!("{}", result_string);
+        });
+        println!("{}", &res.join(""));
     }
 }
--- src/b.rs
+++ src/b.rs
@@ -1,47 +1,42 @@
 use std::io;
 
-fn sort_by_cond(a: &(i32, i32), b: &(i32, i32)) -> bool {
-    if a.0 != b.0 {
-        a.0 < b.0
-    } else {
-        a.1 > b.1
-    }
-}
+pub fn main() {
+    let mut t = String::new();
+    io::stdin().read_line(&mut t).expect("Error reading line");
+    let t: u16 = t.trim().parse().unwrap();
 
-fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Error reading line");
-    let mut iter = input.trim().split_whitespace();
-    let n: usize = iter.next().unwrap().parse().unwrap();
-    let k: i32 = iter.next().unwrap().parse().unwrap();
+    for _ in 0..t {
+        let mut nk = String::new();
+        io::stdin().read_line(&mut nk).expect("Error reading line");
+        let nk: Vec<u32> = nk
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
 
-    let mut v: Vec<i32> = Vec::new();
+        let k = nk[1] as usize;
+        let mut res: Vec<usize> = vec![0; k];
+        let mut count_zeros = 0;
 
-    input.clear();
-    io::stdin().read_line(&mut input).expect("Error reading line");
-    for a in input.trim().split_whitespace() {
-        let num: i32 = a.parse().unwrap() % k;
-        v.push(num);
-    }
+        let mut a = String::new();
+        io::stdin().read_line(&mut a).expect("Error reading line");
+        a.split_whitespace().enumerate().for_each(|(i, x)| {
+            let val = (x.parse::<u32>().unwrap() as u32) % k as u32;
+            if val == 0 {
+                count_zeros += 1;
+                print!("{} ", i + 1);
+            } else {
+                if res[val as usize] == 0 {
+                    res[val as usize] = i + 1;
+                }
+            }
+        });
 
-    for (i, &value) in v.iter().enumerate() {
-        if value == 0 {
-            print!("{} ", i + 1);
+        for &pos in &res {
+            // if pos != 0 {
+                print!("{} ", pos);
+            // }
         }
+
+        println!();
     }
-
-    let mut v1: Vec<(i32, i32)> = Vec::new();
-    for (i, &value) in v.iter().enumerate() {
-        v1.push((value, i as i32 + 1));
-    }
-
-    v1.sort_by(|a, b| sort_by_cond(a, b));
-
-    for pair in &v1 {
-        if pair.0 != 0 {
-            print!("{} ", pair.1);
-        }
-    }
-
-    println!();
 }
